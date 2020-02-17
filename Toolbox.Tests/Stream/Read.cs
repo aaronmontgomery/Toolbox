db3290c740cb28c3489073f183c72c04b1eb0e54
aaronmontgomery <aaronmmontgomery@students.abtech.edu>
2020-02-17 18:54:39 -0500

    removed Random

---
+++ Toolbox.Tests/Stream/Read.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Stream
+{
+    [TestFixture]
+    public static class Read
+    {
+        [TestCase()]
+        public static void Read__()
+        {
+            Assert.Fail();
+        }
+    }
+}
