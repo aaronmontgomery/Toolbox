8bb6f65cbf6dc7cf93df470cf22f46d83aaf7b48
aaronmontgomery <aaronmontgomery1989@outlook.com>
2022-10-06 00:18:07 -0400

    added INetworkService and NetworkService

---
+++ Toolbox/Services/INetworkService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace Toolbox
+{
+    public static partial class Services
+    {
+        public interface INetworkService
+        {
+            IAsyncEnumerable<PingReply> PingRange(IEnumerable<IPAddress> subnet);
+        }
+    }
+}
---
+++ Toolbox/Services/NetworkService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Toolbox
+{
+    public static partial class Services
+    {
+        public class NetworkService : INetworkService
+        {
+            public async IAsyncEnumerable<PingReply> PingRange(IEnumerable<IPAddress> subnet)
+            {
+                Ping ping;
+                PingReply pingReply;
+
+                foreach (IPAddress subnetIpAddress in subnet)
+                {
+                    using (ping = new Ping())
+                    {
+                        pingReply = await ping.SendPingAsync(subnetIpAddress);
+                        if (pingReply.Status == IPStatus.Success)
+                        {
+                            yield return pingReply;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
